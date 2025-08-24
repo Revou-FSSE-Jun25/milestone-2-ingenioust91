bd5ee2318a08039e34ab821202db2bc311b890de
ingenioust91 <ingecendrawatisalim@gmail.com>
2025-08-24 11:22:34 +0700

    refactor guessNumber js

--- js/guessNumber.js
+++ js/guessNumber.js
@@ -1,14 +1,82 @@
 let bossNumber = Math.floor(Math.random() * 100) + 1;
+let bossText = document.getElementById("bossText");
+let bossImage = document.getElementById("bossImage");
 document.getElementById("coba").innerHTML = bossNumber;
+let dogNumber = document.getElementById("dogNumber");
+dogNumber.value = "";
+
+dogNumber.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        guessNumber();
+    }
+})
 
 function guessNumber() {
-    let dogNumber = document.getElementById("dogNumber").value;
+    const getRandomLine = (array) => {
+    const index = Math.floor(Math.random() * array.length);
+    return array[index];
+    }
 
-    if (bossNumber > dogNumber) {
-        document.getElementById("bossText").innerHTML = "too low"
+    let line = ""
+    let ChangeimageBoss;
+
+    if (bossNumber > dogNumber.value) {
+        line = getRandomLine(tooLow);
+        ChangeimageBoss = getRandomLine(imgStillFalse);
     }
-    else if (bossNumber < dogNumber) {
-        document.getElementById("bossText").innerHTML = "too high"
+    else if (bossNumber < dogNumber.value) {
+        line = getRandomLine(tooHigh);
+        ChangeimageBoss = getRandomLine(imgStillFalse);
     }
-    else document.getElementById("bossText").innerHTML = "right"
+    else {
+        line = getRandomLine(right);
+        ChangeimageBoss = getRandomLine(imgRight);
+    }
+
+    bossText.textContent = line;
+    bossImage.src = ChangeimageBoss;
 }
+
+function validateInput() {
+    let input = document.getElementById("dogNumber").value;
+    let numberInput = parseInt(input.trim())
+
+    if (isNaN(numberInput)) {
+        document.getElementById("dogNumber").value = "";
+    }
+
+    if (numberInput > 100) {
+        bossText.textContent = "I said between 1 to 100!";
+    }
+}
+
+const tooLow = [
+    "Too low! Come on, that's not complicated",
+    "That’s under my number. I don't like it.",
+    "Not enough. Revise your guess."
+]
+
+const tooHigh = [
+    "Above my number. Don't tell me you can't handle this",
+    "That’s more than what I had in mind. Try again.",
+    "Nope, that’s too much. Revise it."
+]
+
+const right = [
+    "Finally. I know my instructions are clear.",
+    "Good. But I expect faster.",
+    "OK."
+]
+
+const imgStillFalse = [
+    "./assets/boss-mikir-false-1.png",
+    "./assets/boss-mikir-false-2.png",
+    "./assets/boss-mikir-false-3.png",
+    "./assets/boss-mikir-false-4.png"
+]
+
+
+const imgRight = [
+    "./assets/boss-mikir-right-1.png",
+    "./assets/boss-mikir-right-2.png"
+]
