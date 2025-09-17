e3158e12a275d8d9771befd21256dc890a99e9b4
ingenioust91 <ingecendrawatisalim@gmail.com>
2025-09-17 17:21:52 +0700

    catch object

--- src/catchObject.ts
+++ src/catchObject.ts
@@ -10,26 +10,27 @@ function randomX(square : any){
   dropObject(left);
 }
 
-function dropObject(target : HTMLElement) {
+function dropObject(target : HTMLElement,) {
   const duration : number = Math.floor(Math.random() * (3000-1000+1)+1000);
   const containerY : number = ((window.innerHeight * 0.8)-target.clientHeight);
   animate(target,{
     translateY: [0, containerY],
     duration: duration,
     easing: "easeInQuad",
-    // loop: true
     onComplete: function(){randomX(target)}
   });
 }
 
 const countDown = document.getElementById("countDown") as HTMLElement;
-const squares = document.querySelectorAll(".square");
+const squares = document.querySelectorAll(".square") as NodeListOf<HTMLElement>;
 const catcher = document.getElementById("catcher") as HTMLElement;
+let rectCatcher :any;
 let timeLeft : number = 60;
 
 function startGame() {
   squares.forEach(function(square) {
-  randomX(square);})
+  randomX(square);
+  })
 
   let timerId = setInterval(function(){
     countDown.textContent = timeLeft.toString();
@@ -43,12 +44,27 @@ function startGame() {
 }
 
 document.addEventListener("mousemove", e => {
-  const rectCatcher = catcher.getBoundingClientRect(); // untuk ambil posisi realtime catcher
-  console.log("Left:", rectCatcher .left);
+  rectCatcher = catcher.getBoundingClientRect(); // untuk ambil posisi realtime catcher
+  // console.log("catcher:", rectCatcher.left);
+  isCollide(rectCatcher.left, rectCatcher.top, rectCatcher.right, rectCatcher.bottom)
 });
 
-function getScore() {
-  
+const isCollide = (catcherLeft:any, catcherTop:any, catcherRight:any, catcherBottom:any) => {
+squares.forEach(square => {
+    const rectSquare = square.getBoundingClientRect();
+
+    const collide = !(
+      catcherRight < rectSquare.left ||
+      catcherLeft> rectSquare.right ||
+      catcherBottom < rectSquare.top ||
+      catcherTop > rectSquare.bottom
+    );
+
+    if (collide) {
+      console.log("masuk");
+    }
+  });
 }
+
 
 startGame();
