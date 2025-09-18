42c2ca9db640deb457973dcc36b93be688a6d4c2
ingenioust91 <ingecendrawatisalim@gmail.com>
2025-09-18 17:21:00 +0700

    add matchPair ts

--- src/matchPair.ts
+++ src/matchPair.ts
@@ -1,21 +1,22 @@
 import { utils, animate, stagger } from 'animejs';
 
 const cards = utils.$('.card');
-const x = stagger('3.2rem');
+const x = stagger('6.4rem');
 
 // Initial squares x position
 utils.set(cards, { x });
 
 const shuffle = () => animate(utils.shuffle(cards), { x });
 const card = document.querySelectorAll(".card") as NodeListOf<HTMLElement>;
+const countDown = document.getElementById("countDown") as HTMLElement;
+let timeLeft : number = 60;
 
 const flipCard = (eachCard : HTMLElement):void => {
     animate(eachCard,{
-        scale: [{value:1}, {value:1.3},{value:1, delay: 250} ],
-        rotateY: [{value: "+=180"}, {delay: 200}],
-        easing: "easeInOutSine",
-        duration: 400,
-    })
+    rotateY: [0,180,0], //supaya balik nutup lagi
+    easing: 'easeInOutQuad',
+    duration: 2000,
+  });
 }
 
 const startGame = () => {
@@ -23,8 +24,20 @@ const startGame = () => {
 
     card.forEach(function(value : HTMLElement){
         value.addEventListener("click", function(){
-            flipCard(value)})
+            flipCard(value);
+            
+            })
     })
+
+    let timerId = setInterval(function(){
+    countDown.textContent = timeLeft.toString();
+    timeLeft--;
+
+    if (timeLeft < 0) {
+        clearInterval(timerId);
+        countDown.textContent = "TIME'S UP!";
+    }
+  },1000)
 }
 
 startGame();
